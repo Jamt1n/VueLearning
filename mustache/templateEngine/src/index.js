93c74578b91325e61a13f33a3f79007c2284b122
lujunting <1316560980@qq.com>
2021-06-18 00:08:15 +0800

    feat:收集token

--- mustache/templateEngine/src/index.js
+++ mustache/templateEngine/src/index.js
@@ -1,25 +1,30 @@
 import Scanner from './Scanner';
+import parseTemplateToTokens from './parseTemplateToTokens';
 
 window.templateEngine = {
   render(templateStr, data) {
     // 1. 命令Scanner工作
 
     // 实例化扫描器
-    let scanner = new Scanner(templateStr);
+    // let scanner = new Scanner(templateStr);
 
     // // 测试
     // let words = scanner.scanUntil('{{')
     // console.log(scanner.pos);
     // console.log(words);
-    let word;
-    while(!scanner.eos()) {
-      word = scanner.scanUntil("{{")
-      console.log(word);
-      scanner.scan("{{")
+
+    // let word;
+    // while(!scanner.eos()) {
+    //   word = scanner.scanUntil("{{")
+    //   console.log(word);
+    //   scanner.scan("{{")
       
-      word = scanner.scanUntil("}}");
-      console.log(word);
-      scanner.scan("}}")
-    }
+    //   word = scanner.scanUntil("}}");
+    //   console.log(word);
+    //   scanner.scan("}}")
+    // }
+
+
+    let tokens = parseTemplateToTokens(templateStr);
   }
 }
---
+++ mustache/templateEngine/src/parseTemplateToTokens.js
@@ -0,0 +1,36 @@
+import Scanner from './Scanner';
+
+export default function parseTemplateToTokens(templateStr) {
+  let tokens = [];
+
+  // 实例化扫描器
+  let scanner = new Scanner(templateStr);
+
+  let word;
+  while(!scanner.eos()) {
+    // 收集开始标记
+    word = scanner.scanUntil("{{")
+    if(word != '') {
+      tokens.push(['text', word])
+      console.log(word);
+    }
+    scanner.scan("{{")
+    
+    word = scanner.scanUntil("}}");
+    if(word != '') {
+      if(word[0] == "#") {
+        // 懂下标为1开始存
+        tokens.push(['#', word.substring(1)])
+      }else if (word[0] == '/') {
+        tokens.push(['/', word.substring(1)])
+      }else {
+        console.log(word);
+        tokens.push(['text', word])
+      }
+    }
+    scanner.scan("}}")
+  }
+
+  console.log(tokens);
+  return tokens;
+}
