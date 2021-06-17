53742e0fd2fc9ac821ec0bcf0a52b7a208692c6b
lujunting <1316560980@qq.com>
2021-06-17 23:27:16 +0800

    feat:实现Scanner类

---
+++ mustache/templateEngine/src/Scanner.js
@@ -0,0 +1,38 @@
+/* 
+  扫描器类
+*/
+
+export default class Scanner{
+  constructor(templateStr) {
+    console.log(templateStr);
+    this.templateStr = templateStr;
+    // 指针
+    this.pos = 0;
+    // 尾巴
+    this.tail = templateStr;
+  }
+
+  // 路过，没返回值
+  scan(tag) {
+    if(this.tail.indexOf(tag) == 0) {
+      // tag多长就移几位 比如{{移动2位
+      this.pos += tag.length;
+      // 尾要变
+      this.tail = this.templateStr.substring(this.pos)
+    }
+  }
+
+  // 指针扫描，直到遇到指定内容，返回路过的文字
+  scanUntil(stopTag) {
+    // 记录执行本方法的pos
+    const pos_backup = this.pos;
+    // 尾的头不是stopTap就是没扫描到
+    while(this.tail.indexOf(stopTag) != 0 && this.pos < this.templateStr.length) {
+      this.pos++;
+      // 尾从当前字符开始
+      this.tail = this.templateStr.substr(this.pos)
+    }
+    
+    return this.templateStr.substring(pos_backup, this.pos)
+  }
+}
--- mustache/templateEngine/src/index.js
+++ mustache/templateEngine/src/index.js
@@ -1,3 +1,25 @@
-import * as haha from './haha.js'
+import Scanner from './Scanner';
 
-haha.ha();
+window.templateEngine = {
+  render(templateStr, data) {
+    // 1. 命令Scanner工作
+
+    // 实例化扫描器
+    let scanner = new Scanner(templateStr);
+
+    // // 测试
+    // let words = scanner.scanUntil('{{')
+    // console.log(scanner.pos);
+    // console.log(words);
+    let word;
+    while(scanner.pos != templateStr.length) {
+      word = scanner.scanUntil("{{")
+      console.log(word);
+      scanner.scan("{{")
+      
+      word = scanner.scanUntil("}}");
+      console.log(word);
+      scanner.scan("}}")
+    }
+  }
+}
