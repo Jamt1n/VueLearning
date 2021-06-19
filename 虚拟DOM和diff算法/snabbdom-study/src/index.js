9ff82c16562b504b943e7238ca33e331ba2f8c46
lujunting <1316560980@qq.com>
2021-06-19 17:05:06 +0800

    feat:h函数嵌套使用

--- 虚拟DOM和diff算法/snabbdom-study/src/index.js
+++ 虚拟DOM和diff算法/snabbdom-study/src/index.js
@@ -19,8 +19,13 @@ const myVnode2 = h("div", { class: { box: true } }, "我是一个盒子");
 const myVnode3 = h("ul", [
   h("li", "苹果"),
   h("li", "西瓜"),
-  h("li", "香蕉"),
-  h("li", "火龙果")
+  h("li", [
+    h("div", [
+      h("p", "哈哈"),
+      h("p", "嘻嘻"),
+    ])
+  ]),
+  h("li", h("p", "火龙果"))
 ]);
 // 让虚拟节点上树
 let container = document.querySelector(".container");
