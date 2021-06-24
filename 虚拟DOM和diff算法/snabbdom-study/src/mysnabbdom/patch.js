49e711c8b77b7100a1f7b50a291391bfd3c5dd4d
lujunting <1316560980@qq.com>
2021-06-25 01:08:29 +0800

    fix:上樹bug

--- 虚拟DOM和diff算法/snabbdom-study/src/mysnabbdom/patch.js
+++ 虚拟DOM和diff算法/snabbdom-study/src/mysnabbdom/patch.js
@@ -2,7 +2,7 @@ import vnode from "./vnode";
 import createElement from "./createElement";
 import patchVnode from "./patchVnode";
 
-export default function patch(oldVnode, newVode) {
+export default function patch(oldVnode, newVnode) {
   // 判断传入的第一个参数，是DOM节点还是虚拟节点?
   if (oldVnode.sel == "" || oldVnode.sel == undefined) {
     //    传入的第一个参数是DOM节点，此时要包装为虚拟节点
@@ -15,15 +15,15 @@ export default function patch(oldVnode, newVode) {
     );
   }
   //判断oldVnode和newVnode是不是同一个节点
-  if (oldVnode.key == newVode.key && oldVnode.sel == newVode.sel) {
+  if (oldVnode.key == newVnode.key && oldVnode.sel == newVnode.sel) {
     // 精细比较 ！！最复杂！！
-    patchVnode(oldVnode, newVode);
+    patchVnode(oldVnode, newVnode);
   } else {
     //    暴力插入新的，删除旧的
-    let newVdomElm = createElement(newVode);
+    let newVnodeElm = createElement(newVnode);
     // 插入到老节点之前
-    if (oldVnode.elm.parentNode && newVdomElm) {
-      oldVnode.elm.parentNode.insertBefore(newVdomElm, oldVnode.elm);
+    if (oldVnode.elm.parentNode && newVnodeElm) {
+      oldVnode.elm.parentNode.insertBefore(newVnodeElm, oldVnode.elm);
     }
     // 删除老节点
     oldVnode.elm.parentNode.removeChild(oldVnode.elm);
--- 虚拟DOM和diff算法/snabbdom-study/src/mysnabbdom/patchVnode.js
+++ 虚拟DOM和diff算法/snabbdom-study/src/mysnabbdom/patchVnode.js
@@ -1,17 +1,17 @@
 import createElement from "./createElement";
 
-export default function patchVnode(oldVnode, newVode) {
+export default function patchVnode(oldVnode, newVnode) {
 //  判断新旧vnode是否同一节点
-    if (oldVnode === newVode) return;
+    if (oldVnode === newVnode) return;
     // 判断新vnode有没有text属性
     if (
-        newVode.text != undefined && (newVode.children == undefined ||
-        newVode.children.length == 0)
+        newVnode.text != undefined && (newVnode.children == undefined ||
+        newVnode.children.length == 0)
     ) {
         // 新vnode有text属性
-        if (newVode.text != oldVnode.text) {
+        if (newVnode.text != oldVnode.text) {
             // 如果新vnode和老的vnode的text不同，直接新的写入老的
-            oldVnode.elm.innerText = newVode.text;
+            oldVnode.elm.innerText = newVnode.text;
         }
     } else {
         // 新vnode没有text属性，有children
@@ -19,41 +19,31 @@ export default function patchVnode(oldVnode, newVode) {
             // 新老都有children
             // 所有未处理节点的开头
             let un = 0;
-            for (let i = 0; i < newVode.children.length; i++) {
-                let ch = newVode.children[i];
-                // 再次遍历，看看欧oldVnode中有没有节点和它的same的
+            for (let i = 0; i < newVnode.children.length; i++) {
+                let ch = newVnode.children[i];
+                 // 再次遍历，看看欧oldVnode中有没有节点和它的same的
                 let isExist = false;
                 for (let j = 0; j < oldVnode.children.length; j++) {
-                    if (oldVnode.children[j].sel == ch.sel && oldVnode.children[j].k == ch.key) {
+                    if (oldVnode.children[j].sel == ch.sel && oldVnode.children[j].key == ch.key) {
                         isExist = true;
                     }
                 }
                 if (!isExist) {
-                    console.log(ch);
-                    let dom = createElement(ch)
+                    console.log(ch)
+                    let dom = createElement(ch);
                     ch.elm = dom;
-                    if (un < oldVnode.children.length) {
-                        oldVnode.elm.insertBefore(dom, oldVnode.children[un].elm)
-                    }else {
-                        oldVnode.elm.appendChild(dom)
-                    }
-
+                    oldVnode.elm.insertBefore(dom, oldVnode.children[un].elm)
                 }else {
-                    // 让处理的指针下移
                     un++;
-                    if (i == j) {
-
-                    }
                 }
             }
-
         } else {
             // 老的没有children，新的有、新的放进老的
             //清空老节点内容
             oldVnode.elm.innerHTML = "";
             // 遍历新节点的children，创建dom 上树
-            for (let i = 0; i < newVode.children.length; i++) {
-                let dom = createElement(newVode.children[i]);
+            for (let i = 0; i < newVnode.children.length; i++) {
+                let dom = createElement(newVnode.children[i]);
                 oldVnode.elm.appendChild(dom);
             }
         }
