95edb3cfe83e6208be84f7879d16f0a6e5e6ce08
lujunting <1316560980@qq.com>
2021-06-25 00:17:11 +0800

    feat:patchVnode完成上树

--- 虚拟DOM和diff算法/snabbdom-study/src/index.js
+++ 虚拟DOM和diff算法/snabbdom-study/src/index.js
@@ -1,23 +1,22 @@
 import h from "./mysnabbdom/h";
 import patch from "./mysnabbdom/patch";
 
-let myVnode1 = h("ul", {}, [
-  h("li", {}, "E"),
-  h("li", {}, "A"),
-  h("li", {}, "B"),
-  h("li", {}, [
-    h("div", {}, [h("ol", {}, [h("li", {}, "haha"), h("li", {}, "xixi")])]),
-  ]),
-  h("li", {}, "D"),
+const myVnode1 = h("ul", {}, [
+  h("li", { key: "A" }, "A"),
+  h("li", { key: "B" }, "B"),
+  h("li", { key: "C" }, "C"),
 ]);
 
 const container = document.querySelector(".container");
 const btn = document.querySelector("#btn");
 patch(container, myVnode1);
 
-let myVnode2 = h("section", {}, [
-  h("h1", {}, "我是新的h1"),
-  h("h2", {}, "我是新的h2"),
+const myVnode2 = h("ul", {}, [
+  h("li", { key: "A" }, "A"),
+  h("li", { key: "B" }, "B"),
+  h("li", { key: "M" }, "M"),
+  h("li", { key: "N" }, "N"),
+  h("li", { key: "C" }, "C"),
 ]);
 
 btn.onclick = function () {
--- 虚拟DOM和diff算法/snabbdom-study/src/mysnabbdom/patchVnode.js
+++ 虚拟DOM和diff算法/snabbdom-study/src/mysnabbdom/patchVnode.js
@@ -2,11 +2,11 @@ import createElement from "./createElement";
 
 export default function patchVnode(oldVnode, newVode) {
 //  判断新旧vnode是否同一节点
-    if (newVode === newVode) return;
+    if (oldVnode === newVode) return;
     // 判断新vnode有没有text属性
     if (
-        (newVode.text != undefined && newVode.children == undefined) ||
-        newVode.children.length == 0
+        newVode.text != undefined && (newVode.children == undefined ||
+        newVode.children.length == 0)
     ) {
         // 新vnode有text属性
         if (newVode.text != oldVnode.text) {
@@ -14,9 +14,39 @@ export default function patchVnode(oldVnode, newVode) {
             oldVnode.elm.innerText = newVode.text;
         }
     } else {
-        // 新vnode没有text属性
-        if (oldVnode.children != undefined && oldVnode.children > 0) {
+        // 新vnode没有text属性，有children
+        if (oldVnode.children != undefined && oldVnode.children.length > 0) {
             // 新老都有children
+            // 所有未处理节点的开头
+            let un = 0;
+            for (let i = 0; i < newVode.children.length; i++) {
+                let ch = newVode.children[i];
+                // 再次遍历，看看欧oldVnode中有没有节点和它的same的
+                let isExist = false;
+                for (let j = 0; j < oldVnode.children.length; j++) {
+                    if (oldVnode.children[j].sel == ch.sel && oldVnode.children[j].k == ch.key) {
+                        isExist = true;
+                    }
+                }
+                if (!isExist) {
+                    console.log(ch);
+                    let dom = createElement(ch)
+                    ch.elm = dom;
+                    if (un < oldVnode.children.length) {
+                        oldVnode.elm.insertBefore(dom, oldVnode.children[un].elm)
+                    }else {
+                        oldVnode.elm.appendChild(dom)
+                    }
+
+                }else {
+                    // 让处理的指针下移
+                    un++;
+                    if (i == j) {
+
+                    }
+                }
+            }
+
         } else {
             // 老的没有children，新的有、新的放进老的
             //清空老节点内容
