835aa965fa01aace05ae9ea5902b73cea4938f96
lujunting <1316560980@qq.com>
2021-07-11 13:22:10 +0800

    feat:实现使用栈形成AST

--- AST抽象语法树/study-ast/src/parse.js
+++ AST抽象语法树/study-ast/src/parse.js
@@ -11,7 +11,7 @@ export default function (templateStr) {
   let wordRegExp = /^([^\<]+)\<\/[a-z]+[1-6]?\>/;
   // 准备两个栈
   let stack1 = [];
-  let stack2 = [];
+  let stack2 = [{'children': []}];
 
   while (index < templateStr.length - 1) {
     rest = templateStr.substring(index);
@@ -19,14 +19,20 @@ export default function (templateStr) {
       let tag = rest.match(startRegExp)[1];
       console.log("检测到开始标志", tag);
       stack1.push(tag);
-      stack2.push([]);
+      stack2.push({'tag':tag, "children":[]});
       // 指针移动标签的长度+2，为什么要+2，因为<>占两位
       index += tag.length + 2;
     } else if (endRegExp.test(rest)) {
       let tag = rest.match(endRegExp)[1];
       console.log("检测到结束标志", tag);
-      if (tag == stack1[stack1.length - 1]) {
-        stack1.pop();
+      let pop_tag = stack1.pop();
+      // 此时tag一定是和栈1顶部相同的
+      if (tag == pop_tag) {
+        let pop_arr = stack2.pop();
+        if (stack2.length>0) {
+            stack2[stack2.length-1].children.push(pop_arr);
+        }
+
       } else {
         throw new Error(stack1[stack1.length - 1] + "标签没有闭封");
       }
@@ -35,7 +41,9 @@ export default function (templateStr) {
       let word = rest.match(wordRegExp)[1];
       if (!/^\s+$/.test(word)) {
         // 不是全是空
-        console.log("检测到文字");
+        console.log("检测到文字", word);
+        // 推入到stack2栈顶
+        stack2[stack2.length-1].children.push({'text':word, 'type':3});
       }
 
       index += word.length;
@@ -43,6 +51,7 @@ export default function (templateStr) {
       index++;
     }
   }
-
-  console.log(stack1, stack2);
+  // 此时stack2就是我们之前默认放置的一项，返回这一项即可
+  console.log(stack2[0].children[0])
+  return stack2[0].children[0];
 }
